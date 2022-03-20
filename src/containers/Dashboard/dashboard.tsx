8ed798894220a45a4e93e4fdbf82ce4356dfb8d2
Alireza-ce <rezaei.alireza.ce@gmail.com>
2022-03-20 18:03:59 +0330

    add drawer layout

--- src/containers/Dashboard/dashboard.tsx
+++ src/containers/Dashboard/dashboard.tsx
@@ -1,8 +1,21 @@
+import Drawer from '@material-ui/core/Drawer';
 import React from 'react'
+import classes from './panel-layout.module.scss';
 
 function DashboardPage() {
   return (
-    <div>dashboard</div>
+    <div className={classes.layout}>
+      <Drawer anchor="left" variant='permanent' className={classes.drawer} >
+        <div className={classes.drawerContent}>
+          <p>dashboard</p>
+          <p>dashboard</p>
+        </div>
+      </Drawer>
+      <div className={classes.layoutContent}>
+        {/* toolbar */}
+        {/* outlet */}
+      </div>
+    </div>
   )
 }
 
